f06055bd769d35d518e6dec750e3ea7359b57ca4
Pelochus <anghflores@gmail.com>
2023-01-31 19:30:51 +0100

    Modified template, prepared OS & Software scripts

--- os/os-config.sh
+++ os/os-config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# Title: template-script.sh
+# Title: os-config.sh
 # Author: Pelochus
 # Version: 1.0
-# Brief: Template script for the other scripts that will automate the installation and configuration of my server
+# Brief: Automates OS configuration for my home server
 
 #############
 ### Functions
@@ -15,9 +15,10 @@ menu() {
     echo "  ### MENU - Choose an option"
     echo 
     echo "  [1] Do everything"
-    echo "  [2] Do something"
-    echo "  [3] Do something 2"
-    echo "  [4] Exit"
+    echo "  [2] Enable SWAP (8 GB)"
+    echo "  [3] Set automatic system updates and automatic reboots in cron"
+    echo "  [4] Enable 3D Hardware acceleration in Armbian for Orange Pi 5"
+    echo "  [5] Exit"
     echo 
     echo "-------------------------------------------------------------------------------------------------"
 }
@@ -25,21 +26,32 @@ menu() {
 options() {
     case $1 in 
     
-        # Do something
+        # Enable 8 GB SWAP
+        # Also sets swappiness to a low value so that swap is not used often
         2 | 1)
-            ls -l
-            sleep 2
+            
         ;;& 
-        # This "&" makes it possible to execute following options. Used for executing 1 and 2 when 3 is the input
 
-        # Do something 2
+        # Adds automatic system updates and reboots to cron
+        # Auto update every TODO
+        # Reboots every TODO
         3 | 1)
-            echo "I'm doing something 2"
-            sleep 2
+
+        ;;&
+
+        # Enables 3D HW acceleration for Orange Pi 5
+        # See https://www.armbian.com/orangepi-5/
+        # There is a note below download options
+        # Might be unnecessary in the future or in other distros, check
+        4 | 1)
+            sudo add-apt-repository ppa:liujianfeng1994/panfork-mesa
+            sudo add-apt-repository ppa:liujianfeng1994/rockchip-multimedia
+            sudo apt update
+            sudo apt dist-upgrade
         ;;
 
         # Exit
-        4)
+        5)
             echo
             echo "-----------"
 	        echo "Exiting now"
@@ -63,12 +75,12 @@ then
     if [ $1 == '-h' ] || [ $1 == '--help' ]
     then
         echo
-        echo "pelochus-homelab Template Script Help"
+        echo "pelochus-homelab OS Config Script Help"
         echo
         echo "-a / --auto: Runs all options that doesn't require user's help. Needs root privileges"
         echo "-h / --help: Shows this help screen"
         echo 
-        echo "For more information visit https://github.com/Pelochus/pelochus-homelab"
+        echo "For more information visit https://github.com/Pelochus/pelochus-homelab/os"
         echo
         exit 
     elif [ $1 == '-a' ] || [ $1 == '--auto' ] 
@@ -85,8 +97,8 @@ fi
 
 echo
 echo "-------------------------------------------------------------------------------------------------"
-echo "This is a template script, does nothing"
-echo "The scripts for automating the server probably will need root privileges for doing certain things"
+echo "This is a script for auto configuring various OS parameters"
+echo "This script will need root privileges for doing certain things"
 echo "If you are running this script as sudo, you can continue, else the script will fail"
 echo "Are you running as sudo? [Y/N]"
 echo "-------------------------------------------------------------------------------------------------"
--- software/software-install.sh
+++ software/software-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# Title: template-script.sh
+# Title: software-install.sh
 # Author: Pelochus
 # Version: 1.0
-# Brief: Template script for the other scripts that will automate the installation and configuration of my server
+# Brief: Installs software for my server and, if possible, configures the basics
 
 #############
 ### Functions
@@ -15,9 +15,15 @@ menu() {
     echo "  ### MENU - Choose an option"
     echo 
     echo "  [1] Do everything"
-    echo "  [2] Do something"
-    echo "  [3] Do something 2"
-    echo "  [4] Exit"
+    echo "  [2] Install Pi-Hole (needs user interaction)"
+    echo "  [3] Install PiVPN (needs user interaction)"
+    echo "  [4] Install Netdata (needs compiling, may take a while)"
+    echo "  [5] Install qBittorrent"
+    echo "  [6] Install OpenMediaVault"
+    echo "  [7] Install Jellyfin"
+    echo "  [8] Install Minecraft Server"
+    echo "  [9] Install Organizr"
+    echo "  [0] Exit"
     echo 
     echo "-------------------------------------------------------------------------------------------------"
 }
@@ -25,21 +31,49 @@ menu() {
 options() {
     case $1 in 
     
-        # Do something
+        # Install Pi-Hole, needs user interaction, must be done before PiVPN
+        # Configuration is manual, restore a backup or do manually in webUI
         2 | 1)
-            ls -l
-            sleep 2
+            
         ;;& 
-        # This "&" makes it possible to execute following options. Used for executing 1 and 2 when 3 is the input
 
-        # Do something 2
+        # Install PiVPN, also needs user interaction
         3 | 1)
-            echo "I'm doing something 2"
-            sleep 2
+            
+        ;;&
+
+        # Install Netdata, needs compiling, may take a while
+        4 | 1)
+            
+        ;;&
+
+        # Install qBittorrent-nox, which is a CLI edition with webUI
+        5 | 1)
+            
+        ;;&
+
+        # Install OpenMediaVault [NOTE: Port 80 may conflict with PiHole, change OMV port]
+        6 | 1)
+            
+        ;;&
+
+        # TODO Install Jellyfin [maybe add Radarr, Sonarr and Prowlarr to this section]
+        7 | 1)
+            
+        ;;&
+
+        # TODO Install Minecraft server [Either PaperMC/Purpur or that one guide with Docker on jamesachamber] 
+        8 | 1)
+            
+        ;;&
+
+        # TODO Install Organizr [NOTE: May also conflict with PiHole port 80, better change PiHole in this case]
+        9 | 1)
+            
         ;;
 
         # Exit
-        4)
+        0)
             echo
             echo "-----------"
 	        echo "Exiting now"
@@ -63,12 +97,12 @@ then
     if [ $1 == '-h' ] || [ $1 == '--help' ]
     then
         echo
-        echo "pelochus-homelab Template Script Help"
+        echo "pelochus-homelab Software Installation Script Help"
         echo
         echo "-a / --auto: Runs all options that doesn't require user's help. Needs root privileges"
         echo "-h / --help: Shows this help screen"
         echo 
-        echo "For more information visit https://github.com/Pelochus/pelochus-homelab"
+        echo "For more information visit https://github.com/Pelochus/pelochus-homelab/software"
         echo
         exit 
     elif [ $1 == '-a' ] || [ $1 == '--auto' ] 
@@ -85,8 +119,8 @@ fi
 
 echo
 echo "-------------------------------------------------------------------------------------------------"
-echo "This is a template script, does nothing"
-echo "The scripts for automating the server probably will need root privileges for doing certain things"
+echo "This is a script for automating installation and configuration of various softwares for my server"
+echo "The script needs root privileges since it uses apt"
 echo "If you are running this script as sudo, you can continue, else the script will fail"
 echo "Are you running as sudo? [Y/N]"
 echo "-------------------------------------------------------------------------------------------------"
